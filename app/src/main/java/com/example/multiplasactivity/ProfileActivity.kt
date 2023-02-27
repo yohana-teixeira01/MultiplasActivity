a688e4c96a9b2b2e4ce5613a7dec43a0ddc6f127
yohana-teixeira01 <yteixeira133@gmail.com>
2023-02-27 13:36:53 -0300

    Subindo Arquivos

---
+++ app/src/main/java/com/example/multiplasactivity/ProfileActivity.kt
@@ -0,0 +1,25 @@
+package com.example.multiplasactivity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.multiplasactivity.databinding.ActivityMainBinding
+import com.example.multiplasactivity.databinding.ActivityProfileBinding
+
+class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonVoltar.setOnClickListener {
+            /* val i = Intent(this,MainActivity::class.java)
+             startActivity(i)*/
+           // startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+
+    }
+}
